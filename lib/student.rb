7c7b096cac71d8197dc0dd4aa2797eedfa28a17c
Cory M <cmarker4@gmail.com>
2021-01-17 19:59:52 -0600

    done

--- lib/student.rb
+++ lib/student.rb
@@ -5,20 +5,20 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(id = nil, name, grade)
-    @name = name
-    @grade = grade
-  end
+  # def initialize(id = nil, name, grade)
+  #   @name = name
+  #   @grade = grade
+  # end
 
-  def self.create_table
-    sql = <<-SQL
-      CREATE TABLE students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade INTEGER)
-    SQL
-    DB[:conn].execute(sql)
-  end
+  # def self.create_table
+  #   sql = <<-SQL
+  #     CREATE TABLE students (
+  #     id INTEGER PRIMARY KEY,
+  #     name TEXT,
+  #     grade INTEGER)
+  #   SQL
+  #   DB[:conn].execute(sql)
+  # end
 
   def initialize(id = nil, name, grade)
     @name = name
